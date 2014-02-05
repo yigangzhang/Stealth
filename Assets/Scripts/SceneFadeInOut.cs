442405c4daee680f8c557fa46a2f975889fe8f20
Sam Zhang <goneybb@gmail.com>
2014-02-05 15:42:42 -0500

    add fadeInOut

---
+++ Assets/Scripts/SceneFadeInOut.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour 
+{
+	public float fadeSpeed = 1.5f;
+
+	private bool sceneStarting = true;
+
+	void Awake() 
+	{
+		guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+	}
+
+	void Update()
+	{
+		if(sceneStarting)
+			StartScene();
+	}
+
+	void FadeToClear()
+	{
+		guiTexture.color = Color.Lerp(guiText.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeToBlack()
+	{
+		guiTexture.color = Color.Lerp(guiText.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+
+	void StartScene()
+	{
+		FadeToClear();
+
+		if(guiTexture.color.a <= 0.05f)
+		{
+			guiTexture.color = Color.clear;
+			guiTexture.enabled = false;
+			sceneStarting = false;
+		}
+	}
+
+	public void EndScene() 
+	{
+		guiTexture.enabled = true;
+		FadeToBlack();
+
+		if(guiTexture.color.a >= 0.95f)
+			Application.LoadLevel(1);
+	}
+}
