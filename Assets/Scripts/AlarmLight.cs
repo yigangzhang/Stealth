804f33a6657e3cc69d118ba13cc200157a7eac51
unknown <TE2@.(none)>
2014-02-05 15:13:45 -0500

    add alarm light, add tags

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+	public float fadeSpeed = 2f;
+	public float highIntensity = 2f;
+	public float lowIntensity = 0.5f;
+	public float changeMargin = 0.2f;
+	public bool alarmOn;
+
+	private float targetIntensity;
+
+	void Awake()
+	{
+		light.intensity = 0f;
+		targetIntensity = highIntensity;
+	}
+
+	void Update()
+	{
+		if(alarmOn)
+		{
+			light.intensity = Mathf.Lerp(light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+			CheckTargetIntensity();
+		}
+		else
+		{
+			light.intensity = Mathf.Lerp(light.intensity, 0f, fadeSpeed * Time.deltaTime);
+		}
+	}
+
+	void CheckTargetIntensity() 
+	{
+		if(Mathf.Abs(targetIntensity - light.intensity) < changeMargin)
+		{
+			if(targetIntensity == highIntensity)
+				targetIntensity = lowIntensity;
+			else
+				targetIntensity = highIntensity;
+		}
+	}
+}
