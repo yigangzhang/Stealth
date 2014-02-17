7eea25846bf5a3c4a24940c1adc1e2d137e92504
SamZhangGame <goneybb@gmail.com>
2014-02-17 18:10:33 -0500

    add sweep animation to all cctv cameras

---
+++ Assets/KeyPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPickup : MonoBehaviour {
+	public AudioClip key;
+
+	private GameObject player;
+	private PlayerInverntory playerInverntory;
+
+	void Awake()
+	{
+//		player = GameObject(Tags.player);
+//		playerInverntory = player.GetComponent<PlayerInventory>();
+	}
+
+//	void OntriigerEnter(){
+//
+//	}
+}
---
+++ Assets/Scripts/Player/PlayerInverntory.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInverntory : MonoBehaviour 
+{
+	public bool hasKey;
+}
